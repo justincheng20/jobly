46beb9968a9d94637c3bb3c8cb0c964b04d8a43a
justincheng20 <justincheng20@gmail.com>
2020-02-19 10:06:49 -0800

    Added unit test for sqlPartialUpdate

--- __tests__/unit/partialUpdate.test.js
+++ __tests__/unit/partialUpdate.test.js
@@ -1,9 +1,17 @@
+const sqlForPartialUpdate = require('../../helpers/partialUpdate');
+
 describe("partialUpdate()", () => {
   it("should generate a proper partial update query with just 1 field",
       function () {
 
     // FIXME: write real tests!
-    expect(false).toEqual(true);
+    let items = {num_employees: 0};
+    let resp = sqlForPartialUpdate('companies', items, "handle", "1");
+
+    expect(resp).toEqual({
+      query: 
+      'UPDATE companies SET num_employees=$1 WHERE handle=$2 RETURNING *',
+      values: [0, "1"]});
 
   });
 });
